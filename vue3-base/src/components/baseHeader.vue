937ccc4105487c33bb8963ca66de545c401e1759
kouichi.hoshi <kouichi.hoshi@gmail.com>
2022-06-24 12:03:51 +0900

    env修正

--- vue3-base/src/components/baseHeader.vue
+++ vue3-base/src/components/baseHeader.vue
@@ -1,9 +1,13 @@
 <script setup>
 import logoMark from '../components/logoMark.vue'
+const siteTitle = import.meta.env.VITE_SITE_TITLE
+const siteText = import.meta.env.VITE_SITE_TEXT
 </script>
 <template>
   <header class="base-header">
     <logoMark />
+    <h1>{{ siteTitle }}</h1>
+    <p>{{ siteText }}</p>
   </header>
 </template>
 
